fb833ca97ee236ca8b1424b7e52268f8b5ff8918
NR <nroshak@teksavvy.com>
2016-06-03 10:23:56 -0400

    Create hrtstrip.py

---
+++ hrtstrip.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 15:29:39 2016
+
+@author: nroshak
+ganked from http://stackoverflow.com/questions/20203101/removing-n-in-between-double-quotes-in-a-csv-unix
+"""
+
+import sys, csv, os
+
+
+def hrtstrip (inputfile,outputfile,newtext):
+    print("Input file " + inputfile)
+    print("Output file " + outputfile)
+    with open(inputfile, "r") as input:
+       with open(outputfile, "w") as output:
+          w = csv.writer(output, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
+          for record in csv.reader(input):
+             w.writerow(tuple(s.replace("\n", newtext) for s in record))
+    print("All done")      
